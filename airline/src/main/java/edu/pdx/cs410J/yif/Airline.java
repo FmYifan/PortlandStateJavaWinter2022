2cf758869360b0d7cb3abf3ccfbe5993609dbb26
FmYifan <yifanzhao4@gmail.com>
2022-01-12 11:53:54 -0800

    1/12 koans update && project 1 update

--- airline/src/main/java/edu/pdx/cs410J/yif/Airline.java
+++ airline/src/main/java/edu/pdx/cs410J/yif/Airline.java
@@ -2,13 +2,16 @@ package edu.pdx.cs410J.yif;
 
 import edu.pdx.cs410J.AbstractAirline;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Airline extends AbstractAirline<Flight> {
   private final String name;
+  private ArrayList<Flight> flightList;
 
   public Airline(String name) {
     this.name = name;
+    flightList = new ArrayList<>();
   }
 
   @Override
@@ -19,10 +22,16 @@ public class Airline extends AbstractAirline<Flight> {
   @Override
   public void addFlight(Flight flight) {
     throw new UnsupportedOperationException("This method is not implemented yet");
+    /*
+    flightList.add(flight);
+     */
   }
 
   @Override
   public Collection<Flight> getFlights() {
     throw new UnsupportedOperationException("This method is not implemented yet");
+    /*
+
+     */
   }
 }
--- airline/src/main/java/edu/pdx/cs410J/yif/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/yif/Flight.java
@@ -3,28 +3,61 @@ package edu.pdx.cs410J.yif;
 import edu.pdx.cs410J.AbstractFlight;
 
 public class Flight extends AbstractFlight {
+
+  private int number;
+  private String source;
+  private String departureString;
+  private String destination;
+  private String arrivalString;
+
+  public Flight(){
+  }
+
+  public Flight(int number, String source, String departureString, String destination, String arrivalString){
+    this.number = number;
+    this.source = source;
+    this.departureString = departureString;
+    this.destination = destination;
+    this.arrivalString = arrivalString;
+  }
+
   @Override
   public int getNumber() {
     return 42;
+    /*
+    return number;
+     */
   }
 
   @Override
   public String getSource() {
     throw new UnsupportedOperationException("This method is not implemented yet");
+    /*
+    return source;
+     */
   }
 
   @Override
   public String getDepartureString() {
     throw new UnsupportedOperationException("This method is not implemented yet");
+    /*
+    return departureString;
+     */
   }
 
   @Override
   public String getDestination() {
     throw new UnsupportedOperationException("This method is not implemented yet");
+    /*
+    return destination;
+     */
   }
 
   @Override
   public String getArrivalString() {
     throw new UnsupportedOperationException("This method is not implemented yet");
+    /*
+    return arrivalString;
+     */
   }
 }
